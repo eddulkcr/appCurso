b150c1159f6673ce77dc852477101858898865ba
Eduardo Jaramillo <eddu_lkcr@outlook.es>
2017-08-14 17:29:49 -0500

    cambios del componente copyright

--- src/app/copyright/copyright.component.ts
+++ src/app/copyright/copyright.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CopyrightComponent implements OnInit {
 
+  copyright: string = 'acme S.A';
+  hoy: any = new Date();
+
   constructor() { }
 
   ngOnInit() {
